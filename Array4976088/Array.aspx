26ccedec3caa3ce229c9ce736030b12cd1b76fdf
DELL <DELL@DAYANNASIXCO>
2025-02-26 23:51:22 -0600

    Primer commit

---
+++ Array4976088/Array.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Array.aspx.cs" Inherits="Array4976088.Array" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="Create Category Array"></asp:Label>
+            <br />
+            <asp:TextBox ID="strCategoryArray" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="btnCreateArray" runat="server" OnClick="btnCreateArray_Click" Text="Create Array" Width="130px" />
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Array4976088/Array.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Array4976088
+{
+    public partial class Array : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCreateArray_Click(object sender, EventArgs e)
+        {
+            //Declare and load the array
+            String[] strCategoryArray = new string[5];
+            strCategoryArray[0] = "Footwear - Men  's' ";
+            strCategoryArray[1] = "Clothing - Men  's' ";
+            strCategoryArray[2] = "Racquets - Women 's' ";
+            strCategoryArray[3] = "Footwear - Women 's' ";
+            strCategoryArray[4] = " Clothing - Women 's' ";
+
+            Session["strCategoryArray"] = strCategoryArray;
+
+
+            Response.Redirect("CategoryUseArraySessionVariables.aspx");
+
+
+
+        }
+    }
+}
